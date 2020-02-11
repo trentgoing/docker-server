c7f7eec44be05c9d411c7ecef4ea420bfb1fa970
Trent Going <trent.going@gmail.com>
2020-02-11 15:06:33 -0500

    Initialization of the needed server components

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = 4444;
+
+const { Client } = require('pg');
+const client = new Client({
+  user: 'postgres',
+  host: 'name-i-gave-my-postgres-container-in-the-docker-compose-file',
+  database: 'postgres',
+  port: 5432,
+})
+client.connect();
+
+
+app.get('/', (req, res) => {
+  client.query('SELECT * FROM team;', (err, dbResponse) => {
+    if (err) {
+      res.status(400);
+      res.send('Database is not connected successfully!');
+    }
+    res.send(dbResponse);
+    client.end()
+  });
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
